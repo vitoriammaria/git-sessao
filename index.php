d2f80090887e0ba5855cd9d36642137f08d21190
vitoriammaria <vitoriamariaquijad@gmail.com>
2023-10-16 10:04:28 -0300

    Criação do Index.php + Criação do teste H1

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SESSÃO</title>
+</head>
+<body>
+    <h1>TESTE - 01</h1>
+    
+</body>
+</html>
