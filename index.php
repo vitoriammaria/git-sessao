6ca018dd19d98b3dbee57fb60ddb0106ae8c6aff
vitoriammaria <vitoriamariaquijad@gmail.com>
2023-10-16 11:17:58 -0300

    Inicial

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>SESSÃO</title>
+    <title>Index</title>
 </head>
 <body>
     <h1>TESTE - 01</h1>
